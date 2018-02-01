5befdf752b8b9a8b542af99785a66c6b210f08b1
Karley Mackay <karleynmackay@gmail.com>
2018-02-01 11:36:05 -0800

    first change

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -8,8 +8,12 @@
 ##   * Version
 ##   * Priority
 ##   * Built
+packages <- installed.packages() %>%
+  as_tibble()
 
 ## write it to data/installed-packages.csv
+write.csv(packages, file = "data/installed-packages.csv")
+
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
